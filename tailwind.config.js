3fa0d729ffdf8ef9578c67f604750254a0521c4c
Sushant <sushantbhagat561@gmail.com>
2025-10-09 11:12:58 +0530

    fixing some bugs for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   darkMode: 'class',
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require('@tailwindcss/forms')],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
